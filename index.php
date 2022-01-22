51bd6cb0205fd416e6c2c2bb2fd095979fa63a88
PellegattaSimone <pellegattasimone@outlook.com>
2022-01-22 15:38:27 +0100

    Added style.css and script.js

--- index.php
+++ index.php
@@ -19,50 +19,19 @@
 		<!--Style-->
 		<link rel="stylesheet" href="<?=ROOT?>/style.css" type="text/css" media="all">
 
-		<!--Browsers-->
+		<!--Javascript-->
+		<script src="script.js"></script>
+
+		<!--Favicon-->
 		<link rel="icon" href="<?=ROOT?>/favicon/favicon.ico" type="image/x-icon">
 		<link rel="icon" href="<?=ROOT?>/favicon/favicon-16x16.png" type="image/png" sizes="16x16">
 		<link rel="icon" href="<?=ROOT?>/favicon/favicon-32x32.png" type="image/png" sizes="32x32">
 		<link rel="icon" href="<?=ROOT?>/favicon/favicon-48x48.png" type="image/png" sizes="48x48">
 		<link rel="icon" href="<?=ROOT?>/favicon/favicon-96x96.png" type="image/png" sizes="96x96">
-		<link rel="icon" href="<?=ROOT?>/favicon/favicon.svg" type="image/svg+xml">
 		<link rel="manifest" href="<?=ROOT?>/manifest.webmanifest">
 
 		<!--Preload-->
 		<!--link rel="preload" as="image" imagesrcset=" 392w,  722w" imagesizes="(min-width: 61.25em) 722px, 392px"-->
-
-		<style>
-			body {
-				margin: 0;	/*full screen*/
-			}
-			#container {
-				display: flex;
-				flex-flow: row nowrap;
-
-				width: 100%;	/*temp*/
-				aspect-ratio: 16/9;	/*tv aspect ratio*/
-			}
-			#map {
-				height: 100%;	/*only way to make aspect ratio work*/
-			}
-			#map img {
-				height: 100%;
-				aspect-ratio: 6/5;	/*this could change*/
-			}
-			#secondary {
-				flex-grow: 1;	/*for #container (parent)*/
-
-				display: flex;
-				flex-flow: column nowrap;
-			}
-			#park {
-				flex-grow: 1;	/*this can grow (if different ratio for #map img)*/
-				min-height: 0;	/*this can shrink*/
-			}
-			#hologram {
-				aspect-ratio: 1/1;	/*temp: not mandatory*/
-			}
-		</style>
 	</head>
 
 	<body>
