004e4993e1c4a6b91c262fcf3f2caf60fd118691
Jacopo8N <jaco0416nava@gmail.com>
2022-02-02 13:10:18 +0100

    Initialized Point class

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ class Point {
     }
 }
 
+let main = new Point(291,541);
 let _1 = new Point(292,727);
 let _2 = new Point(474,606);
 let _3 = new Point(1,1);
@@ -13,3 +14,5 @@ let _5 = new Point(1,1);
 
 let points = [_1, _2, _3, _4, _5];
 
+var circle = document.createElement("span");
+circle.classList.add("main");
