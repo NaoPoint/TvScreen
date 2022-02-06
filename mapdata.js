02d997dc66119eaa06565664675b74dcbbb09437
PellegattaSimone <pellegattasimone@outlook.com>
2022-02-07 00:18:23 +0100

    Removed semicolons from JS
    I just discovered that semicolons are optional on JavaScript

--- mapdata.js
+++ mapdata.js
@@ -1,22 +1,22 @@
 function setPoints() {
 	//map points:
-	let points = [];	//array containing all the points
+	let points = []	//array containing all the points
 
-	points.push(new Point(16.1,54.8));	//naopoint
-	points.push(new Point(16.1,74.4));	//1
-	points.push(new Point(27,61.4));	//2
-	points.push(new Point(47.4,61.4));	//3
-	points.push(new Point(70.2,61.4));	//4
-	points.push(new Point(78,73.6));	//5
+	points.push(new Point(16.1,54.8))	//naopoint
+	points.push(new Point(16.1,74.4))	//1
+	points.push(new Point(27,61.4))	//2
+	points.push(new Point(47.4,61.4))	//3
+	points.push(new Point(70.2,61.4))	//4
+	points.push(new Point(78,73.6))	//5
 
-	return points;
+	return points
 }
 
 function setPaths() {
-	let connections = [];
+	let connections = []
 
 	//naopoint - 1
 	x = [points[0].horizontal, 20]
-	y = [64,8, 74,4, points[1].vertical];
-	connections[0] = new Connection(points[0], points[1], false, x, y);	//vertical
+	y = [64,8, 74,4, points[1].vertical]
+	connections[0] = new Connection(points[0], points[1], false, x, y)	//vertical
 }
--- script.js
+++ script.js
@@ -1,46 +1,46 @@
 //single point on the map:
 class Point {
     constructor(horizontal, vertical) {	//percentage coordinates
-        this.horizontal = horizontal;
-        this.vertical = vertical;
+        this.horizontal = horizontal
+        this.vertical = vertical
     }
 }
 
 class Connection {
-    totalLength = 0;
+    totalLength = 0
 
     constructor(start, end, axis, x, y) {    //array of every turn in the connection
-        console.log(this.totalLength);
+        console.log(this.totalLength)
         
         if(axis) {  //true = horizontal
-            this.totalLength += this.calculateLength(x);
-            this.totalLength += this.calculateLength(y);
+            this.totalLength += this.calculateLength(x)
+            this.totalLength += this.calculateLength(y)
 
-            console.log(this.totalLength);
+            console.log(this.totalLength)
         }
     }
 
     calculateLength(array) {
-        let length = 0;
+        let length = 0
         
         for (let i = 0; i < length(array); i++)
-            length += Math.abs(array[i] - array[i + 1]);
+            length += Math.abs(array[i] - array[i + 1])
         
-        return length;
+        return length
     }
 }
 
-points = setPoints();	//set points inside mapdata.js
-points = setPaths();	//set points inside mapdata.js
+points = setPoints()	//set points inside mapdata.js
+paths = setPaths()	//set points inside mapdata.js
 
 //point marker:
 for (const point of points) {
-	let marker = document.createElement("span");	//virtual element
-	marker.classList.add("marker");	//for css
+	let marker = document.createElement("span")	//virtual element
+	marker.classList.add("marker")	//for css
 
 	//set marker position (already absolute)
-	marker.style.left = point.horizontal + "%";
-	marker.style.top = point.vertical + "%";
+	marker.style.left = point.horizontal + "%"
+	marker.style.top = point.vertical + "%"
 
-	document.body.appendChild(marker);	//assign virtual element to body
+	document.body.appendChild(marker)	//assign virtual element to body
 }
