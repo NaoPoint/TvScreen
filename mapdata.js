35e5088a1f63b22399c04da10edb6b94d78fee44
PellegattaSimone <pellegattasimone@outlook.com>
2022-02-07 01:17:02 +0100

    Upgraded to SASS
    Updated style to SASS preprocessor, fixed JavaScript Connection class

--- mapdata.js
+++ mapdata.js
@@ -2,21 +2,24 @@ function setPoints() {
 	//map points:
 	let points = []	//array containing all the points
 
-	points.push(new Point(16.1,54.8))	//naopoint
-	points.push(new Point(16.1,74.4))	//1
-	points.push(new Point(27,61.4))	//2
-	points.push(new Point(47.4,61.4))	//3
-	points.push(new Point(70.2,61.4))	//4
-	points.push(new Point(78,73.6))	//5
+	points.push(new Point(16.1, 54.8))	//naopoint
+	points.push(new Point(16.1, 74.4))	//1
+	points.push(new Point(27, 61.4))	//2
+	points.push(new Point(47.4, 61.4))	//3
+	points.push(new Point(70.2, 61.4))	//4
+	points.push(new Point(78, 73.6))	//5
 
 	return points
 }
 
 function setPaths() {
+	//map connections
 	let connections = []
 
 	//naopoint - 1
-	x = [points[0].horizontal, 20]
-	y = [64,8, 74,4, points[1].vertical]
-	connections[0] = new Connection(points[0], points[1], false, x, y)	//vertical
+	x = [20]
+	y = [64.8, 74.4]
+	connections.push(new Connection(points[0], points[1], false, x, y))	//vertical
+
+	return connections
 }
--- script.js
+++ script.js
@@ -4,43 +4,70 @@ class Point {
         this.horizontal = horizontal
         this.vertical = vertical
     }
+
+	static displayPoints(points) {
+		//point marker:
+		for (const point of points) {
+			let marker = document.createElement("span")	//virtual element
+			marker.classList.add("point marker")	//for css
+
+			//set marker position (already absolute)
+			marker.style.left = point.horizontal + "%"
+			marker.style.top = point.vertical + "%"
+
+			document.body.appendChild(marker)	//assign virtual element to body
+		}
+	}
 }
 
 class Connection {
     totalLength = 0
 
     constructor(start, end, axis, x, y) {    //array of every turn in the connection
-        console.log(this.totalLength)
+		//start at the beginning, end at the end
+		x.unshift(start.horizontal)
+		x.push(end.horizontal)
+		y.unshift(start.vertical)
+		y.push(end.vertical)
         
-        if(axis) {  //true = horizontal
-            this.totalLength += this.calculateLength(x)
-            this.totalLength += this.calculateLength(y)
+		this.totalLength += this.calculateLength(x)	//length on the x axis
+		this.totalLength += this.calculateLength(y)	//length on the y axis
 
-            console.log(this.totalLength)
-        }
+        if(axis) {  //true = horizontal first in the path, false = vertical first
+
+        } else {
+
+		}
+
+		console.log(this.totalLength)	//temp
     }
 
     calculateLength(array) {
         let length = 0
         
-        for (let i = 0; i < length(array); i++)
+        for (let i = 0; i < array.length - 1; i++)
             length += Math.abs(array[i] - array[i + 1])
         
-        return length
+		return length.toFixed(2)*1	//round to 2 decimals
     }
+
+	static displayConnections(connections) {
+		//point marker:
+		for (const connection of connections) {
+			let marker = document.createElement("span")	//virtual element
+			marker.classList.add("connection marker")	//for css
+
+			//set marker position (already absolute)
+			marker.style.left = point.horizontal + "%"
+			marker.style.top = point.vertical + "%"
+
+			document.body.appendChild(marker)	//assign virtual element to body
+		}
+	}
 }
 
 points = setPoints()	//set points inside mapdata.js
 paths = setPaths()	//set points inside mapdata.js
 
-//point marker:
-for (const point of points) {
-	let marker = document.createElement("span")	//virtual element
-	marker.classList.add("marker")	//for css
-
-	//set marker position (already absolute)
-	marker.style.left = point.horizontal + "%"
-	marker.style.top = point.vertical + "%"
-
-	document.body.appendChild(marker)	//assign virtual element to body
-}
+Point.displayPoints(points)
+Point.displayPoints(paths)
