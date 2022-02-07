aefc7794b1f7de7a5d7ad7e62196e9983726ff0f
PellegattaSimone <pellegattasimone@outlook.com>
2022-02-07 01:49:46 +0100

    Added JSON graph for python

--- mapdata.js
+++ mapdata.js
@@ -2,12 +2,12 @@ function setPoints() {
 	//map points:
 	let points = []	//array containing all the points
 
-	points.push(new Point(16.1, 54.8))	//naopoint
-	points.push(new Point(16.1, 74.4))	//1
-	points.push(new Point(27, 61.4))	//2
-	points.push(new Point(47.4, 61.4))	//3
-	points.push(new Point(70.2, 61.4))	//4
-	points.push(new Point(78, 73.6))	//5
+	points.push(new Point("naopoint", 17.1, 56.8))	//naopoint
+	points.push(new Point("1", 17.1, 76.4))	//1
+	points.push(new Point("2", 28, 64.4))	//2
+	points.push(new Point("3", 48.4, 63.4))	//3
+	points.push(new Point("4", 71.2, 64.4))	//4
+	points.push(new Point("5", 79, 75.8))	//5
 
 	return points
 }
@@ -17,8 +17,8 @@ function setPaths() {
 	let connections = []
 
 	//naopoint - 1
-	x = [20]
-	y = [64.8, 74.4]
+	x = [21]
+	y = [64.9, 76.5]
 	connections.push(new Connection(points[0], points[1], false, x, y))	//vertical
 
 	return connections
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //single point on the map:
 class Point {
-    constructor(horizontal, vertical) {	//percentage coordinates
+    constructor(name, horizontal, vertical) {	//percentage coordinates
+		this.name = name
         this.horizontal = horizontal
         this.vertical = vertical
     }
@@ -9,7 +10,7 @@ class Point {
 		//point marker:
 		for (const point of points) {
 			let marker = document.createElement("span")	//virtual element
-			marker.classList.add("point marker")	//for css
+			marker.classList.add("point", "marker")	//for css
 
 			//set marker position (already absolute)
 			marker.style.left = point.horizontal + "%"
@@ -24,6 +25,11 @@ class Connection {
     totalLength = 0
 
     constructor(start, end, axis, x, y) {    //array of every turn in the connection
+		this.start = start.name	//e.g. 1,2,3,naopoint
+		this.end = end.name
+		this.x = x
+		this.y = y
+
 		//start at the beginning, end at the end
 		x.unshift(start.horizontal)
 		x.push(end.horizontal)
@@ -53,21 +59,36 @@ class Connection {
 
 	static displayConnections(connections) {
 		//point marker:
-		for (const connection of connections) {
-			let marker = document.createElement("span")	//virtual element
-			marker.classList.add("connection marker")	//for css
+		for (const connection of connections)
+			for (const x of connection.x)
+				for (const y of connection.y) {
+					let marker = document.createElement("span")	//virtual element
+					marker.classList.add("connection", "marker")	//for css
 
-			//set marker position (already absolute)
-			marker.style.left = point.horizontal + "%"
-			marker.style.top = point.vertical + "%"
+					//set marker position (already absolute)
+					marker.style.left = x + "%"
+					marker.style.top = y + "%"
 
-			document.body.appendChild(marker)	//assign virtual element to body
-		}
+					document.body.appendChild(marker)	//assign virtual element to body
+				}
 	}
 }
 
-points = setPoints()	//set points inside mapdata.js
-paths = setPaths()	//set points inside mapdata.js
+let points = setPoints()	//set points inside mapdata.js
+let paths = setPaths()	//set points inside mapdata.js
 
 Point.displayPoints(points)
-Point.displayPoints(paths)
+Connection.displayConnections(paths)
+
+//create json graph for python
+graph = {}
+for (const connection of paths) {
+	let start = connection.start
+
+	if (!(start in graph))	//if subgraph not defined yet
+		graph[start] = {}	//subgraph for a single point
+	
+	graph[start][connection.end] = connection.totalLength	//assign cost to graph connection
+}
+
+json = JSON.stringify(graph)	//text to send to python
