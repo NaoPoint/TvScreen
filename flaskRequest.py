06d95a2cfa7eecb9f1ac45fefe3e6bff92aacf1d
PellegattaSimone <pellegattasimone@outlook.com>
2022-04-22 02:27:23 +0200

    Conformed flaskRequest to Choregraphe script

--- flaskRequest.py
+++ flaskRequest.py
@@ -10,14 +10,20 @@ def updateMap(num = None):
 		dictToSend = str(randrange(1, 9))	#update using random number
 
 	json.loads(dictToSend)	#convert to json
+	output = None
 
 	try:
 		res = requests.post('http://192.168.1.100:5000/update', json=dictToSend)	#flask endpoint
-		dictFromServer = res.json()
+		output = res.json()	#success
+
 	except requests.ConnectionError as e:
-		dictFromServer = e	#connection refused / aborted
+		output = "Connection error: " + str(e)	#connection refused / aborted
+
+	except Exception as e:
+		output = "Unknown error: " + str(sys.exc_info()[0])	#unexpected
+
 	finally:
-		print(dictFromServer)	#print result (false if error)
+		print(output)	#print result (success or error)
 
 if __name__ == "__main__":	#if in main module (not imported)
 	if(len(sys.argv) - 1):	#if there are script arguments
